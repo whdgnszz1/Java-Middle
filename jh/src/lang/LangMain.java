92b18d6bfa411039ac604bdf443a4c18f5f7bb64
홍종훈 <whdgnszz1@snu.ac.kr>
2024-05-08 22:28:02 +0900

    Lang package

---
+++ jh/src/lang/LangMain.java
@@ -0,0 +1,26 @@
+package lang;
+/***************************************************
+ * Copyright(c) 2021-2023 Kyobo Book Centre All right reserved.
+ * This software is the proprietary information of Kyobo Book.
+ *
+ * Revision History
+ * Author                         Date          Description
+ * --------------------------     ----------    ----------------------------------------
+ * Jonghun Hong                   5/8/24
+ *
+ ****************************************************/
+
+
+/**
+ * @author : Jonghun Hong
+ * @Project : jh
+ * @FileName : LangMain
+ * @Date : 5/8/24
+ * @description :
+ */
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
