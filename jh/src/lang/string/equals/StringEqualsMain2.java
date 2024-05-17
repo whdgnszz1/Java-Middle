557f9adaa71b724cd5e8516bd4f7048cc6560713
홍종훈 <whdgnszz1@snu.ac.kr>
2024-05-17 22:52:52 +0900

    String class compare

---
+++ jh/src/lang/string/equals/StringEqualsMain2.java
@@ -0,0 +1,38 @@
+package lang.string.equals;
+/***************************************************
+ * Copyright(c) 2021-2023 Kyobo Book Centre All right reserved.
+ * This software is the proprietary information of Kyobo Book.
+ *
+ * Revision History
+ * Author                         Date          Description
+ * --------------------------     ----------    ----------------------------------------
+ * Jonghun Hong                   5/17/24
+ *
+ ****************************************************/
+
+
+/**
+ * @author : Jonghun Hong
+ * @Project : jh
+ * @FileName : StringEqualsMain2
+ * @Date : 5/17/24
+ * @description :
+ */
+
+public class StringEqualsMain2 {
+
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+        System.out.println("메서드 호출 비교1: " + isSame(str1, str2));
+
+        String str3 = "hello";
+        String str4 = "hello";
+        System.out.println("메서드 호출 비교2: " + isSame(str3, str4));
+    }
+
+    private static boolean isSame(String x, String y) {
+        // return x == y;
+        return x.equals(y);
+    }
+}
