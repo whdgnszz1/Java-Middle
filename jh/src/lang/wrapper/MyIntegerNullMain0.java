8b9d594b3c3d2ae5c7cade3a8d61317254333535
홍종훈 <whdgnszz1@snu.ac.kr>
2024-05-23 08:46:43 +0900

    Limit of primitive type

---
+++ jh/src/lang/wrapper/MyIntegerNullMain0.java
@@ -0,0 +1,40 @@
+package lang.wrapper;
+/***************************************************
+ * Copyright(c) 2021-2023 Kyobo Book Centre All right reserved.
+ * This software is the proprietary information of Kyobo Book.
+ *
+ * Revision History
+ * Author                         Date          Description
+ * --------------------------     ----------    ----------------------------------------
+ * Jonghun Hong                   5/23/24
+ *
+ ****************************************************/
+
+
+/**
+ * @author : Jonghun Hong
+ * @Project : jh
+ * @FileName : MyIntegerNullMain0
+ * @Date : 5/23/24
+ * @description :
+ */
+
+public class MyIntegerNullMain0 {
+
+    public static void main(String[] args) {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        System.out.println(findValue(intArr, -1));
+        System.out.println(findValue(intArr, 0));
+        System.out.println(findValue(intArr, 1));
+        System.out.println(findValue(intArr, 100));
+    }
+
+    public static int findValue(int[] intArr, int target) {
+        for (int value : intArr) {
+            if (value == target) {
+                return value;
+            }
+        }
+        return -1;
+    }
+}
